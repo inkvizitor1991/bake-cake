474b8045f334fd9622c173f918cef6a087dec5de
rimprog <riminprog@gmail.com>
2022-04-29 18:53:59 +0300

    Fixed pass_cake_ingridients

--- cake/views.py
+++ cake/views.py
@@ -44,14 +44,14 @@ class AccountViews(views.View):
         title = 'Личный кабинет'
         context = {
             'title': title,
-            'user': user,
+            'user': user.first_name,
             'email': user.email,
             'client': client,
         }
         if client:
             context = {
                 'title': title,
-                'user': user,
+                'user': user.first_name,
                 'email': user.email,
                 'client': client,
                 'orders': client.orders.all()
@@ -162,6 +162,43 @@ def payment(request):
     return redirect(payment.confirmation.confirmation_url)
 
 
+def pass_cake_ingridients(request):
+    levels = Levels.objects.all()
+    forms = Form.objects.all()
+    toppings = Topping.objects.all()
+    berries = Berries.objects.all()
+    decors = Decor.objects.all()
+
+    cake_components_serialized = {
+        'levels': {
+            'quantity': [level.quantity for level in levels],
+            'prices': [level.price for level in levels]
+        },
+        'forms': {
+            'figures': [form.figure for form in forms],
+            'prices': [form.price for form in forms]
+        },
+        'toppings': {
+            'names': [topping.name for topping in toppings],
+            'prices': [topping.price for topping in toppings]
+        },
+        'berries': {
+            'names': [berry.name for berry in berries],
+            'prices': [berry.price for berry in berries]
+        },
+        'decors': {
+            'names': [decor.name for decor in decors],
+            'prices': [decor.price for decor in decors]
+        }
+    }
+
+    response = JsonResponse(
+        cake_components_serialized,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
+
+    return response
+
 
 def register_order(request):
     try:
@@ -223,6 +260,8 @@ def register_order(request):
             delivery=delivery,
             comment=order_raw['Comments']
         )
+
+        login(request, user)
 
         return JsonResponse({
             'order': order_raw,
